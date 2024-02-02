2895a966459066d0faab31a5417a0e60fbd7ed61
Patrick Harrington <harringtonpaccounts@proton.me>
2024-02-02 11:46:05 -0800

    Added read and print

---
+++ read_and_print.c
@@ -0,0 +1,37 @@
+/*
+    Read and Print -- written to understand the basics of reading and printing the contents of a file.
+    Intended to serve as a building block to later process the contents of a file. The file being
+    read is explicitly given in the fopen() function and its contents are simply printed to the
+    terminal. Also, use of the errno.h library provides some experience in error handling.
+
+    Written by: Pat Harrington
+    Date: 2/2/2024
+*/
+
+#include <stdio.h>
+#include <errno.h> // For user with perror() in line 13
+
+int main() {
+    FILE *file;
+    char buffer[100]; // Maximum number of characters read in a single operation
+
+    // Open numbers.txt and read file
+    file = fopen("numbers.txt", "r");
+
+    // Error handling for no file/directory found
+    if (file == NULL) {
+        perror("");
+        return 1;
+    }
+
+    // Read file contents and print to terminal
+    while (fread(buffer, sizeof(char), sizeof(buffer), file) > 0) {
+        printf("%s", buffer);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return 0;
+    
+}
